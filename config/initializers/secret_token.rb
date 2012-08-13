6d5fdc17f8939599aa6d9fe296c2a61ef4db0dc0
andrew cwik <andrew>
2012-08-13 12:38:37 -0500

    Initial commit hubub is born

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hubub::Application.config.secret_token = '632945806bbb9accb28ec61287f8468588fa81e9a1ba490253dfd2f3803e45b344fb921f880b036adaf409db56ead7f3565866c394f6e3e026cfdc2189d5f898'
